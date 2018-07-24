f6e56bdb69bb0e13b3f6701c6392ac9886f7b1a7
Peter O'Connor <peter.ed.oconnor@gmail.com>
2018-07-24 15:15:51 +0200

    added initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "defer-defaults",
+    author = "Peter OConnor, JonathPeter OConnor, Jonathan Fine",
+    version = 0,
+    dependency_links = (),
+    install_requires = [''],
+    scripts = [],
+    packages=find_packages(),
+    )
